03ba834175c6c88070c676b0ce13e5f2c490026f
PFT\pocky_wu <pocky_wu@perfectcorp.com>
2026-05-15 17:48:23 +0800

    Initial commit: 部門聚餐投票系統

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBW3IumCjYl4syM8y47YTuG3XKr3APzFlw",
+  authDomain: "team-gathering-ca3ae.firebaseapp.com",
+  projectId: "team-gathering-ca3ae",
+  storageBucket: "team-gathering-ca3ae.firebasestorage.app",
+  messagingSenderId: "976901661100",
+  appId: "1:976901661100:web:1f52e9a0d21ac63954da09"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
